0e8d1742e23ed4d0846b14171f8e27eada5ac1fb
peterqiu0516 <peterqiu0516@gmail.com>
2021-01-05 09:18:36 +0800

    C1_W1 - Asymptotic Analysisis, Karatsuba with customized high precision calculation (BigNum)

---
+++ Course 1 - Divide and Conquer, Sorting and Searching, and Randomized Algorithms/README.md
@@ -0,0 +1,21 @@
+# Course 1 - Divide and Conquer, Sorting and Searching, and Randomized Algorithms
+
+Materials for the first course of the Algorithms Specialization offered by Stanford on Coursera.
+
+## Course Syllabus
+
++ ### [Week 1: Merge Sort, "big-oh" notation and asymptotic analysis](https://github.com/PeterQiu0516/Stanford-Algorithms/tree/master/Course%201%20-%20%20Divide%20and%20Conquer%2C%20Sorting%20and%20Searching%2C%20and%20Randomized%20Algorithms/Week%201)
+
++ ### Week 2:
+
+
++ ### Week 3: 
+
+  
++ ### Week 4: 
+
+
+
+## About
+
+Course link: https://www.coursera.org/learn/machine-learning-trading-finance/
--- Course 1 - Divide and Conquer, Sorting and Searching, and Randomized Algorithms/Week 1/Programming-Assignment/Karatsuba.cpp
+++ Course 1 - Divide and Conquer, Sorting and Searching, and Randomized Algorithms/Week 1/Programming-Assignment/Karatsuba.cpp
@@ -7,30 +7,30 @@ using namespace std;
 
 const int MAXN = 128; // log(a*b) = log a + log b; 64 digit * 64 digit < 128 digit
 
-class bigNum
+class BigNum
 {
     int a[MAXN] = {0};
 
 public:
-    bigNum();
-    bigNum(string num); // for input
-    bigNum(const bigNum &source, int new_len, int pos); // for cutting down & copy
+    BigNum();
+    BigNum(string num);                                 // for input
+    BigNum(const BigNum &source, int new_len, int pos); // for cutting down & copy
 
     int len;
     void print();
-    bigNum pow(int n); // return this * 10^n
+    BigNum pow(int n); // return this * 10^n
 
-    bigNum operator+(const bigNum &b);
-    bigNum operator-(const bigNum &b);
-    bigNum operator*(const bigNum &b);
+    BigNum operator+(const BigNum &b);
+    BigNum operator-(const BigNum &b);
+    BigNum operator*(const BigNum &b);
 };
 
 // constructor
-bigNum::bigNum() : len(0)
+BigNum::BigNum() : len(0)
 {
 }
 
-bigNum::bigNum(string num) : len(num.size())
+BigNum::BigNum(string num) : len(num.size())
 {
     reverse(num.begin(), num.end());
     for (int i = 0; i < num.size(); i++)
@@ -39,7 +39,7 @@ bigNum::bigNum(string num) : len(num.size())
     }
 }
 
-bigNum::bigNum(const bigNum &source, int new_len, int pos) : len(new_len)
+BigNum::BigNum(const BigNum &source, int new_len, int pos) : len(new_len)
 {
     for (int i = 0; i < new_len; i++)
     {
@@ -48,7 +48,7 @@ bigNum::bigNum(const bigNum &source, int new_len, int pos) : len(new_len)
 }
 
 // method
-void bigNum::print()
+void BigNum::print()
 {
     if (!this->len)
         cout << "0";
@@ -59,9 +59,9 @@ void bigNum::print()
     cout << endl;
 }
 
-bigNum bigNum::pow(int n)
+BigNum BigNum::pow(int n)
 {
-    bigNum ans(*this, this->len, 0); // copy
+    BigNum ans(*this, this->len, 0); // copy
     for (int i = ans.len - 1; i >= 0; i--)
     {
         ans.a[i + n] = ans.a[i];
@@ -74,9 +74,9 @@ bigNum bigNum::pow(int n)
     return ans;
 }
 
-bigNum bigNum::operator+(const bigNum &b)
+BigNum BigNum::operator+(const BigNum &b)
 {
-    bigNum ans;
+    BigNum ans;
     ans.len = max(this->len, b.len) + 1;
 
     for (int i = 0; i < ans.len; i++)
@@ -91,9 +91,9 @@ bigNum bigNum::operator+(const bigNum &b)
     return ans;
 }
 
-bigNum bigNum::operator-(const bigNum &b)
+BigNum BigNum::operator-(const BigNum &b)
 {
-    bigNum ans;
+    BigNum ans;
     ans.len = this->len;
 
     for (int i = 0; i < b.len; i++)
@@ -117,9 +117,9 @@ bigNum bigNum::operator-(const bigNum &b)
 }
 
 // basic multiplication
-bigNum bigNum::operator*(const bigNum &b)
+BigNum BigNum::operator*(const BigNum &b)
 {
-    bigNum ans;
+    BigNum ans;
     for (int i = 0; i < this->len; i++)
     {
         for (int j = 0; j < b.len; j++)
@@ -136,21 +136,21 @@ bigNum bigNum::operator*(const bigNum &b)
 }
 
 // kara
-bigNum kara(bigNum x, bigNum y)
+BigNum kara(BigNum x, BigNum y)
 {
     if (!x.len || !y.len)
-        return bigNum();
+        return BigNum();
     if (x.len == 1 || y.len == 1) // 3*11
         return x * y;
 
     int len_cut = min(x.len / 2, y.len / 2); // 168*1052
-    bigNum a(x, x.len - len_cut, len_cut);   // x = a*10^len_cut + b; x.len-x.len/2 to avoid odd cases (3 digits, 168)
-    bigNum b(x, len_cut, 0);
-    bigNum c(y, y.len - len_cut, len_cut);
-    bigNum d(y, len_cut, 0);
-    bigNum ac = kara(a, c);
-    bigNum bd = kara(b, d);
-    bigNum t = kara((a + b), (c + d));
+    BigNum a(x, x.len - len_cut, len_cut);   // x = a*10^len_cut + b; x.len-x.len/2 to avoid odd cases (3 digits, 168)
+    BigNum b(x, len_cut, 0);
+    BigNum c(y, y.len - len_cut, len_cut);
+    BigNum d(y, len_cut, 0);
+    BigNum ac = kara(a, c);
+    BigNum bd = kara(b, d);
+    BigNum t = kara((a + b), (c + d));
 
     return (ac.pow(len_cut * 2)) + (t - ac - bd).pow(len_cut) + bd;
 }
@@ -184,19 +184,19 @@ int main()
 {
     string x, y;
     cin >> x >> y;
-    bigNum X(x);
-    bigNum Y(y);
+    BigNum X(x);
+    BigNum Y(y);
 
     // basic multiplication
     clock_t startTime1 = clock();
-    bigNum basic_ans = X * Y;
+    BigNum basic_ans = X * Y;
     basic_ans.print();
     clock_t basicTime = clock();
     cout << "Basic multiplication: " << (double)(basicTime - startTime1) / CLOCKS_PER_SEC << " s" << endl;
 
     // karatsuba multiplication
     clock_t startTime2 = clock();
-    bigNum kara_ans = kara(X, Y);
+    BigNum kara_ans = kara(X, Y);
     kara_ans.print();
     clock_t karaTime = clock();
     cout << "Karatsuba multiplication: " << (double)(karaTime - startTime2) / CLOCKS_PER_SEC << " s" << endl;
